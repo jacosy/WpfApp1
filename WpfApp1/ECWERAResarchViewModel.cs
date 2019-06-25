4440c5a0de356151f87c48ca8d62f2fec8ec65d3
Longoria You <longoria.you@gmail.com>
2019-06-25 22:40:33 +0800

    First Commit

---
+++ WpfApp1/ECWERAResarchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace WpfApp1
+{
+    public class ECWERAResarchViewModel
+    {
+        public ECWERAResarchViewModel()
+        {
+            MailboxResponseList = new List<MailboxIDModel>
+            {
+                new MailboxIDModel{ MailboxID="A11111", TaxID="T1111111"},
+                new MailboxIDModel{ MailboxID="A2222", TaxID="T22222"},
+                new MailboxIDModel{ MailboxID="A33333", TaxID="T3333"}
+            };
+        }
+
+        public string SearchMailboxId { get; set; }
+        public List<string> SearchMailboxIdList { get; set; }
+        public string SearchTaxId { get; set; }
+
+        public List<MailboxIDModel> MailboxResponseList { get; set; }
+    }
+}
---
+++ WpfApp1/MailboxID.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfApp1
+{
+    public class MailboxIDModel
+    {
+        public string MailboxID { get; set; }
+        public string TaxID { get; set; }
+    }
+}
